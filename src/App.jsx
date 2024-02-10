03a3a3462f2ae64d7fe33c3c11eb6b5d3c461dc7
yair <yairmasterlol@gmail.com>
2024-02-10 17:11:59 -0600

    2024-02-10-05:11:59 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import Router from "../routes/Router";
-import { useEffect } from "react";
-import axios from "axios";
-import { api } from "../api";
 
 function App() {
-  // useEffect(() => {
-  //   console.log(`${api}/jwt`);
-  //   axios
-  //     .get(`${api}/jwt`)
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-
   return (
     <>
       <Router />
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,29 +1,8 @@
-import QrScanner from "qr-scanner";
-import { useEffect } from "react";
-
 const Test = () => {
   const example = `{"data":"https://www.bourbon.co.jp/petit/" ,"cornerPoints":[{"x":318,"y":494.4},{"x":444,"y":508.79999999999995},{"x":429.59999999999997,"y": 637.2},{"x":301.2,"y":621.5999999999999}]}`;
   const res = JSON.parse(example);
   console.log(res.data);
-  useEffect(() => {
-    const videoElem = document.createElement("video");
-    document.body.appendChild(videoElem);
-    const qrScanner = new QrScanner(
-      videoElem,
-      (result) => {
-        // console.log("decoded qr code:", result);
-        console.log(result);
-        // alert(JSON.stringify(result));
-        // navigator.clipboard.writeText(JSON.stringify(result));
-      },
-      { highlightScanRegion: true }
-    );
-    qrScanner.start();
-    return () => {
-      qrScanner.stop();
-      document.body.removeChild(videoElem);
-    };
-  }, []);
+
   return (
     <div>
       <div>jaja lolas yahooo jajaj</div>
@@ -38,12 +17,6 @@ const Test = () => {
       >
         login
       </button>
-      <video
-        id="video"
-        playsInline
-        className=""
-        style={{ width: "10px", height: "10px" }}
-      />
     </div>
   );
 };
