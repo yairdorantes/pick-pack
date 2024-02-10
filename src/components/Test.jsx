f33218696a7a222fcf2fc5054f16853d188a5dea
yair <yairmasterlol@gmail.com>
2024-02-10 07:10:25 -0600

    2024-02-10-07:10:25 AM

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,8 +1,10 @@
-import { stringify } from "postcss";
 import QrScanner from "qr-scanner";
 import { useEffect } from "react";
 
 const Test = () => {
+  const example = `{"data":"https://www.bourbon.co.jp/petit/" ,"cornerPoints":[{"x":318,"y":494.4},{"x":444,"y":508.79999999999995},{"x":429.59999999999997,"y": 637.2},{"x":301.2,"y":621.5999999999999}]}`;
+  const res = JSON.parse(example);
+  console.log(res.data);
   useEffect(() => {
     const videoElem = document.createElement("video");
     document.body.appendChild(videoElem);
@@ -11,8 +13,8 @@ const Test = () => {
       (result) => {
         // console.log("decoded qr code:", result);
         console.log(result);
-        alert(JSON.stringify(result));
-        navigator.clipboard.writeText(JSON.stringify(result));
+        // alert(JSON.stringify(result));
+        // navigator.clipboard.writeText(JSON.stringify(result));
       },
       { highlightScanRegion: true }
     );
@@ -24,6 +26,7 @@ const Test = () => {
   }, []);
   return (
     <div>
+      <div>jaja</div>
       <video
         id="video"
         playsInline
--- src/views/QR/QrScan.jsx
+++ src/views/QR/QrScan.jsx
@@ -1,65 +1,99 @@
 import axios from "axios";
-import Html5QrcodePlugin from "./Html5QrcodePlugin";
 import { api } from "../../../api";
 import toast from "react-hot-toast";
 import useStore from "../../../Context";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { flushSync } from "react-dom";
 import NavBar from "../../components/NavBar";
+import QrScanner from "qr-scanner";
 
 const QrScan = () => {
+  const qrScannerRef = useRef(null); // Create a useRef for qrScanner
+
   const { user } = useStore();
   const [code, setCode] = useState("");
+  const example = `{"data":"https://www.bourbon.co.jp/petit/" ,"cornerPoints":[{"x":318,"y":494.4},{"x":444,"y":508.79999999999995},{"x":429.59999999999997,"y": 637.2},{"x":301.2,"y":621.5999999999999}]}`;
+  const res = JSON.parse(example);
+  console.log(res.data);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(code);
-    if (code.length > 5) {
-      axios
-        .post(`${api}/pick-pack/assigment/qr`, {
-          userId: user.id,
-          orderId: code,
-        })
-        .then((result) => {
-          console.log(result);
-          toast.success(`Se te ha asignado la order ${code}`, {
-            duration: 3000,
-          });
-          if (!document.startViewTransition) {
-            navigate(`/picking/${code}`);
-            return;
-          }
-          document.startViewTransition(() =>
-            flushSync(() => navigate(`/picking/${code}`))
-          );
-        })
-        .catch((err) => {
-          console.log(err);
-          toast.error("Ups algo salio mal, intenta de nuevo", {});
-          // toast.remove();
-        })
-        .finally(() => setCode(""));
-    }
-  }, [code]);
+  const handleQRCode = () => {
+    axios
+      .post(`${api}/pick-pack/assigment/qr`, {
+        userId: user.id,
+        orderId: code,
+      })
+      .then((result) => {
+        console.log(result);
+        toast.success(`Se te ha asignado la order ${code}`, {
+          duration: 3000,
+        });
+        if (!document.startViewTransition) {
+          navigate(`/picking/${code}`);
+          return;
+        }
+        document.startViewTransition(() =>
+          flushSync(() => navigate(`/picking/${code}`))
+        );
+      })
+      .catch((err) => {
+        console.log(err);
 
-  const onNewScanResult = (decodedText, decodedResult) => {
-    console.log(decodedResult, decodedText);
-    setCode(decodedText);
+        toast.error("Ups algo salio mal, intenta de nuevo", {});
+        // toast.remove();
+      })
+      .finally(() => setCode(""));
   };
+
+  useEffect(() => {
+    const videoElem = document.createElement("video");
+    document.body.appendChild(videoElem);
+
+    const qrScanner = new QrScanner(
+      videoElem,
+      (result) => {
+        console.log(result);
+        setCode(JSON.parse(result));
+      },
+      { highlightScanRegion: true }
+    );
+
+    qrScannerRef.current = qrScanner; // Assign qrScanner to qrScannerRef.current
+
+    qrScanner.start();
+
+    return () => {
+      qrScanner.stop();
+      document.body.removeChild(videoElem);
+    };
+  }, []);
   return (
-    <div className="">
-      <NavBar>
-        <div className="mt-40 ">
-          <Html5QrcodePlugin
-            fps={10}
-            qrbox={250}
-            disableFlip={false}
-            qrCodeSuccessCallback={onNewScanResult}
-          />
-        </div>
-      </NavBar>
-    </div>
+    <NavBar>
+      <button
+        onClick={() => {
+          qrScannerRef.current?.stop();
+        }}
+        className="btn"
+      >
+        stop
+      </button>{" "}
+      <button
+        onClick={() => {
+          qrScannerRef.current?.start();
+        }}
+        className="btn"
+      >
+        activate
+      </button>
+      <div>{code}</div>
+      <video
+        id="video"
+        playsInline
+        className=""
+        style={{ width: "10px", height: "10px" }}
+      />
+    </NavBar>
   );
 };
 
