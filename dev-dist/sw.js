1166a7f5229971921a7cc7f218c0c355d7bfd07f
yair <yairmasterlol@gmail.com>
2024-02-10 09:57:42 -0600

    2024-02-10-09:57:42 AM

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jh4he1e62f"
+    "revision": "0.3ih2taochjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
