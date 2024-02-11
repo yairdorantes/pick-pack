ee0b3065ac41a5c5c688e79ce7bdb08dde97332c
yair <yairmasterlol@gmail.com>
2024-02-10 18:25:20 -0600

    2024-02-10-06:25:20 PM

--- routes/AuthRoutes.jsx
+++ routes/AuthRoutes.jsx
@@ -1,8 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useStore from "../Context";
+import { Suspense } from "react";
 
 const AuthRoutes = () => {
   const { user } = useStore();
-  return user ? <Outlet /> : <Navigate to="/login" />;
+
+  // Suspense fallback for lazy loading
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {user ? <Outlet /> : <Navigate to="/login" />}
+    </Suspense>
+  );
 };
 export default AuthRoutes;
