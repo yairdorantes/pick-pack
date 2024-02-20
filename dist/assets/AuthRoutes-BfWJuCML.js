423bdf4274a535193f0e0e3311e5009835825e43
unknown <yair>
2024-02-20 11:48:10 -0600

    packing assigment feature

---
+++ dist/assets/AuthRoutes-BfWJuCML.js
@@ -0,0 +1,1 @@
+import{j as t,O as o,N as e}from"./index-DDCn74jT.js";import{u as r}from"./Context-31unKQ0j.js";const i=()=>{const{user:s}=r();return s?t.jsx(o,{}):t.jsx(e,{to:"/login"})};export{i as default};
--- dist/assets/AuthRoutes-J2mhVwGH.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o,N as e}from"./index-CkNXQP3F.js";import{u as r}from"./Context-DDDUfGmI.js";const i=()=>{const{user:s}=r();return s?t.jsx(o,{}):t.jsx(e,{to:"/login"})};export{i as default};
