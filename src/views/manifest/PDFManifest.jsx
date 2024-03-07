010c897e83979640d18cc48d6226daee6a7652cd
unknown <yair>
2024-03-07 16:38:34 -0600

    del everthing manifest

--- src/views/manifest/PDFManifest.jsx
+++ src/views/manifest/PDFManifest.jsx
@@ -10,10 +10,11 @@ import toast from "react-hot-toast";
 const PDFManifest = () => {
   const { courierId } = useParams();
   const [modalDelConfirmation, setModalDelConfirmation] = useState(false);
-
+  const [modalDelEverthing, setModalDelEverthing] = useState(false);
   const [base64PDF, setBase64PDF] = useState("");
   const [tableData, setTableData] = useState([]);
   const [loadingPDF, setLoadingPDF] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [modalReceipt, setModalReceipt] = useState(false);
   const [rowSelected, setRowSelected] = useState({});
   const [courier, setCourier] = useState("");
@@ -84,6 +85,22 @@ const PDFManifest = () => {
       setCourier("UPS");
     }
   }, []);
+
+  const delEverthing = () => {
+    console.log("del everthibg");
+    setLoading(true);
+    axios
+      .delete(`${api}/pick-pack/wipe_manifest/${courierId}`)
+      .then(() => {
+        toast.success("Ordenes eliminadas!");
+        setModalDelEverthing(false);
+        setTableData([]);
+      })
+      .catch((err) => {
+        console.log("Ups algo salió mal");
+      })
+      .finally(() => setLoading(false));
+  };
 
   const handleOpenPDF = () => {
     let pdfWindow = window.open("");
@@ -162,7 +179,7 @@ const PDFManifest = () => {
 
       {/* tableData.length > 0 */}
       {tableData.length > 0 && (
-        <div className="text-center mt-10 mb-10 flex justify-center gap-4">
+        <div className="text-center mt-10 mb-10 flex justify-center flex-wrap gap-4">
           <div
             className="px-4 py-2 text-info hover:bg-info hover:text-white border-info border rounded-lg "
             onClick={() => setModalReceipt(true)}
@@ -177,7 +194,7 @@ const PDFManifest = () => {
               <path d="M64 0C28.7 0 0 28.7 0 64v384c0 35.3 28.7 64 64 64h256c35.3 0 64-28.7 64-64v-19.3c-2.7 1.1-5.4 2-8.2 2.7l-60.1 15c-3 .7-6 1.2-9 1.4-.9.1-1.8.2-2.7.2h-64c-6.1 0-11.6-3.4-14.3-8.8l-8.8-17.7c-1.7-3.4-5.1-5.5-8.8-5.5s-7.2 2.1-8.8 5.5l-8.8 17.7c-2.9 5.9-9.2 9.4-15.7 8.8s-12.1-5.1-13.9-11.3L144 381l-9.8 32.8c-6.1 20.3-24.8 34.2-46 34.2H80c-8.8 0-16-7.2-16-16s7.2-16 16-16h8.2c7.1 0 13.3-4.6 15.3-11.4l14.9-49.5c3.4-11.3 13.8-19.1 25.6-19.1s22.2 7.8 25.6 19.1l11.6 38.6c7.4-6.2 16.8-9.7 26.8-9.7 15.9 0 30.4 9 37.5 23.2l4.4 8.8h8.9c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l94.8-94.8V160H256c-17.7 0-32-14.3-32-32V0H64zm192 0v128h128L256 0zm293.8 139.7c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM311.9 321c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5.2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4l129.2-129.3-71-71L311.9 321z" />
             </svg>
           </div>
-
+          {/* 
           <div
             onClick={handleOpenPDF}
             className={`px-4 py-2 text-error cursor-pointer hover:bg-error hover:text-white border-error border rounded-lg ${
@@ -196,9 +213,8 @@ const PDFManifest = () => {
                 <path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8l-6-6zM9.498 16.19c-.309.29-.765.42-1.296.42a2.23 2.23 0 01-.308-.018v1.426H7v-3.936A7.558 7.558 0 018.219 14c.557 0 .953.106 1.22.319.254.202.426.533.426.923-.001.392-.131.723-.367.948zm3.807 1.355c-.42.349-1.059.515-1.84.515-.468 0-.799-.03-1.024-.06v-3.917A7.947 7.947 0 0111.66 14c.757 0 1.249.136 1.633.426.415.308.675.799.675 1.504 0 .763-.279 1.29-.663 1.615zM17 14.77h-1.532v.911H16.9v.734h-1.432v1.604h-.906V14.03H17v.74zM14 9h-1V4l5 5h-4z" />
               </svg>
             )}
-            {/* <-- Modify the text accordingly */}
             {loadingPDF && <span className="loading loading-dots loading-md" />}
-          </div>
+          </div> */}
 
           <div
             className={`px-4 py-2 cursor-pointer text-success hover:bg-success hover:text-white border-success border rounded-lg ${
@@ -233,8 +249,53 @@ const PDFManifest = () => {
               )}
             </a>
           </div>
+          <div
+            onClick={() => setModalDelEverthing(true)}
+            className="px-4 flex items-center py-2 cursor-pointer text-error hover:bg-error hover:text-white border-error border rounded-lg "
+          >
+            <svg
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              className="w-7 h-7"
+              height="1em"
+              width="1em"
+            >
+              <path d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5v7a.5.5 0 01-1 0v-7a.5.5 0 011 0z" />
+            </svg>
+          </div>
         </div>
       )}
+      <Modal isOpen={modalDelEverthing} setIsOpen={setModalDelEverthing}>
+        <div data-theme="" className="card mx-auto  w-full bg-gray-200">
+          <div className="card-body items-center text-center ">
+            <h2 className="text-xl  text-gray-600">
+              ¿Estás segur@ de eliminar <strong>TODAS</strong> las ordenes del
+              manifiesto?
+            </h2>
+            {/* <p>¿Deseas llevar a cabo esta accion?</p> */}
+            <div className="card-actions justify-end">
+              <button
+                onClick={delEverthing}
+                className={`btn ${
+                  loading && "btn-disabled"
+                } btn-success text-white`}
+              >
+                {loading ? (
+                  <span className="loading loading-spinner text-info"></span>
+                ) : (
+                  "Aceptar"
+                )}
+              </button>
+              <button
+                className="btn btn-error text-white"
+                onClick={() => setModalDelEverthing(false)}
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      </Modal>
       <Modal isOpen={modalDelConfirmation} setIsOpen={setModalDelConfirmation}>
         <div data-theme="" className="card mx-auto  w-full bg-gray-200">
           <div className="card-body items-center text-center ">
