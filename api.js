355fa4e31332b173713cf419630b097c93e5b6a7
unknown <yair>
2024-03-15 14:12:55 -0600

    socket in beta mode...

--- api.js
+++ api.js
@@ -2,5 +2,5 @@
 // const url = import.meta.env.VITE_api;
 // export const api = "https://localhost/api";
 export const api = "https://eoms.mx/api";
-export const serverURL = "https://localhost:443";
-// export const serverURL = "https://eoms.mx:443";
+// export const serverURL = "https://localhost:443";
+export const serverURL = "https://eoms.mx:443";
