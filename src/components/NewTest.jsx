7b1373138297ce4c97a536e4af07b0605a523c7f
unknown <yair>
2024-03-15 11:01:16 -0600

    changes save life

--- src/components/NewTest.jsx
+++ src/components/NewTest.jsx
@@ -1,11 +1,17 @@
+import { Scanner } from "@yudiel/react-qr-scanner";
+
 const NewTest = () => {
   return (
     <div>
-      <div>
-        <div>
-          <div></div>
-        </div>
-      </div>
+      <Scanner
+        components={{}}
+        // options={{ delayBetweenScanAttempts: 100 }}
+        onResult={(text, result) => {
+          alert(text, result);
+          console.log(text, result);
+        }}
+        onError={(error) => console.log(error?.message)}
+      />
     </div>
   );
 };
